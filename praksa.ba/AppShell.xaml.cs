44e41a9b487fff26d3b9117fd856863a399cb923
Emir Zambakovic <83666062+zexach@users.noreply.github.com>
2023-01-02 11:51:38 +0100

    Add files via upload

---
+++ praksa.ba/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace praksa.ba;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ praksa.ba/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace praksa.ba;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
